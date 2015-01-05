24aeb0ed74bcaa377e05f4daac67896b8daa1122
Justin Coyne <justin@curationexperts.com>
2015-01-05 12:17:14 -0600

    Disabled access controls in the catalog

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,9 +6,9 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
   include Hydra::Controller::ControllerBehavior
   # These before_filters apply the hydra access controls
-  before_filter :enforce_show_permissions, :only=>:show
+  # before_filter :enforce_show_permissions, :only=>:show
   # This applies appropriate access controls to all solr queries
-  CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  # CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
 
 
   configure_blacklight do |config|
@@ -18,7 +18,7 @@ class CatalogController < ApplicationController
     }
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_tesim'
+    config.index.title_field = 'short_title_tesim'
     config.index.display_type_field = 'has_model_ssim'
 
 
