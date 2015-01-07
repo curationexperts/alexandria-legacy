da5d779b8c2a2790d2d6f5376a47d3dacce75922
Justin Coyne <justin@curationexperts.com>
2015-01-07 13:26:54 -0600

    Don't use a Mongo repo

--- config/initializers/rdf_repositories.rb
+++ config/initializers/rdf_repositories.rb
@@ -1,11 +1,10 @@
 def configure_repositories
   ActiveTriples::Repositories.clear_repositories!
   if Rails.env.test?
-    # ActiveTriples::Repositories.add_repository :default, RDF::Repository.new
     ActiveTriples::Repositories.add_repository :vocabs, RDF::Repository.new
   else
-    # ActiveTriples::Repositories.add_repository :default, RDF::Mongo::Repository.new(:host => ENV['MONGODB_HOST'] || "localhost", :port => ENV["MONGODB_PORT"] || 27017)
-    ActiveTriples::Repositories.add_repository :vocabs, RDF::Mongo::Repository.new(:host => ENV['MONGODB_HOST'] || "localhost", :port => ENV["MONGODB_PORT"] || 27017, :collection => 'vocabs')
+    ActiveTriples::Repositories.add_repository :vocabs, RDF::Repository.new
+    #RDF::Mongo::Repository.new(:host => ENV['MONGODB_HOST'] || "localhost", :port => ENV["MONGODB_PORT"] || 27017, :collection => 'vocabs')
   end
 end
 configure_repositories
