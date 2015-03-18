2892ec42775027d1ddfe85592f9c1199e8392a2f
Justin Coyne <justin@curationexperts.com>
2015-03-18 13:20:16 -0500

    Use activefedora-aggregates

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,31 +1,8 @@
+require 'active_fedora/aggregation'
 class Image < ActiveFedora::Base
   include Metadata
   include Hydra::Collections::Collectible
-
-
-  after_save :save_aggregator
-
-  def save_aggregator
-    generic_files.save
-  end
-
-  # has_one :aggregation
-  # has_many :generic_files, through: :aggregation
-  def generic_files
-    @file_association ||= FileAssociation.new(self, { class_name: 'GenericFile' } )
-  end
-
-  def generic_files=(vals)
-    generic_files.target = vals
-  end
-
-  def generic_file_ids=(vals)
-    generic_files.target_ids = vals
-  end
-
-  def generic_file_ids
-    generic_files.target_ids
-  end
+  include ActiveFedora::Aggregation::Builder
 
   def self.indexer
     ImageIndexer
