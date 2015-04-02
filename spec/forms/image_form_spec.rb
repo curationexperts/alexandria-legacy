5780923981f1d4240e76f946c97374e819e0031f
val99erie <val99erie@gmail.com>
2015-04-02 11:15:55 -0500

    Added series name to edit form. Story #188

--- spec/forms/image_form_spec.rb
+++ spec/forms/image_form_spec.rb
@@ -9,8 +9,12 @@ describe ImageForm do
       expect(subject).to include(location_attributes: [:id, :_destroy])
       expect(subject).to include(lc_subject_attributes: [:id, :_destroy])
       expect(subject).to include(form_of_work_attributes: [:id, :_destroy])
+    end
+
+    it "should include simple fields" do
       expect(subject).to include(sub_location: [])
       expect(subject).to include(use_restrictions: [])
+      expect(subject).to include(series_name: [])
     end
   end
 end
