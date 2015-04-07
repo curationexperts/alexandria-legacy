3e80c19f2d6f7fc0c75f8f5aaecde0a985a7d8ae
Tony Collen <tony@halogenlabs.com>
2015-04-07 13:43:51 -0500

    uniquely name the associations

--- app/models/time_span.rb
+++ app/models/time_span.rb
@@ -7,11 +7,11 @@ class TimeSpan < ActiveFedora::Base
   property :note, predicate: ::RDF::SKOS.note
 
   # temp fix for https://github.com/projecthydra/active_fedora/issues/752 
-  has_many :images, inverse_of: :created
-  has_many :images, inverse_of: :issued
-  has_many :images, inverse_of: :date_other
-  has_many :images, inverse_of: :date_valid
-  has_many :images, inverse_of: :date_copyrighted
+  has_many :created_images, inverse_of: :created
+  has_many :issued_images, inverse_of: :issued
+  has_many :date_other_images, inverse_of: :date_other
+  has_many :date_valid_images, inverse_of: :date_valid
+  has_many :date_copyrighted_images, inverse_of: :date_copyrighted
 
   # MODS date qualifiers
   APPROX = "approximate"
