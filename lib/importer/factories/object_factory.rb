curationexperts/alexandria-legacy

commit 9f4c897f9a3d6fc7f903b8397890cf7733cfc1d1

Fix Dates

lib/importer/factories/object_factory.rb
@@ -19,7 +19,6 @@ class ObjectFactory
 
   def update(obj)
     obj.attributes = transform_attributes.except(:id)
-    before_save(obj)
     obj.save!
     after_save(obj)
     puts "  Updated #{klass.to_s.downcase} #{obj.id} (#{attributes[:accession_number].first})"
@@ -27,10 +26,6 @@ class ObjectFactory
 
   # override after_save if you want to put something here.
   def after_save(obj)
-  end
-
-  # override before_save if you want to put something here.
-  def before_save(obj)
   end
 
   # override after_create if you want to put something here.
@@ -50,7 +45,6 @@ class ObjectFactory
     identifier = mint_ark
     attrs.merge!(identifier: [identifier.id], id: identifier.id.split(/\//).last)
     klass.new(attrs) do |obj|
-      before_save(obj)
       obj.save!
       after_create(obj)
       after_save(obj)

Gemfile
@@ -25,7 +25,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'hydra-head', '9.1.1'
 # gem 'active-fedora', '~> 9.0.7'
-gem 'active-fedora', github: 'projecthydra/active_fedora'
+gem 'active-fedora', github: 'projecthydra/active_fedora', branch: 'fix_inverse_of_has_many'
 
 gem 'active-triples', github: 'jcoyne/ActiveTriples', branch: 'dce-ucsb'
 gem 'hydra-editor', '~> 1.0.3'

app/models/concerns/metadata.rb
@@ -97,15 +97,11 @@ module Metadata
     end
 
     # Dates
-    has_and_belongs_to_many :created, predicate: RDF::DC.created, class_name: 'TimeSpan'
-    has_and_belongs_to_many :issued, predicate: RDF::DC.issued, class_name: 'TimeSpan'
-    has_and_belongs_to_many :date_other, predicate: RDF::DC.date, class_name: 'TimeSpan'
-    has_and_belongs_to_many :date_copyrighted, predicate: RDF::DC.dateCopyrighted, class_name: 'TimeSpan'
-    has_and_belongs_to_many :date_valid, predicate: RDF::DC.valid, class_name: 'TimeSpan'
-
-    # property :issued, predicate: RDF::DC.issued do |index|
-    #   index.as :displayable
-    # end
+    has_and_belongs_to_many :created, predicate: RDF::DC.created, class_name: 'TimeSpan', inverse_of: :images
+    has_and_belongs_to_many :issued, predicate: RDF::DC.issued, class_name: 'TimeSpan', inverse_of: :issued_images
+    has_and_belongs_to_many :date_other, predicate: RDF::DC.date, class_name: 'TimeSpan', inverse_of: :date_other_images
+    has_and_belongs_to_many :date_copyrighted, predicate: RDF::DC.dateCopyrighted, class_name: 'TimeSpan', inverse_of: :date_copyrighted_images
+    has_and_belongs_to_many :date_valid, predicate: RDF::DC.valid, class_name: 'TimeSpan', inverse_of: :date_valid_images
 
     # Not tackling these now. No demonstrated need yet.
     # has_and_belongs_to_many :date_accepted, predicate: RDF::DC.dateAccepted, class_name: 'TimeSpan'

app/models/time_span.rb
@@ -6,12 +6,11 @@ class TimeSpan < ActiveFedora::Base
   property :label, predicate: ::RDF::SKOS.prefLabel
   property :note, predicate: ::RDF::SKOS.note
 
-  # temp fix for https://github.com/projecthydra/active_fedora/issues/752
-  has_many :images, predicate: ::RDF::DC.created, inverse_of: :created
-  has_many :issued_images, inverse_of: :issued
-  has_many :date_other_images, inverse_of: :date_other
-  has_many :date_valid_images, inverse_of: :date_valid
-  has_many :date_copyrighted_images, inverse_of: :date_copyrighted
+  has_many :images, inverse_of: :created, class_name: "Image"
+  has_many :issued_images, inverse_of: :issued, class_name: "Image"
+  has_many :date_other_images, inverse_of: :date_other, class_name: "Image"
+  has_many :date_valid_images, inverse_of: :date_valid, class_name: "Image"
+  has_many :date_copyrighted_images, inverse_of: :date_copyrighted, class_name: "Image"
 
   # MODS date qualifiers
   APPROX = "approximate"

lib/importer/factories/image_factory.rb
@@ -6,7 +6,7 @@ class ImageFactory < ObjectFactory
     Image
   end
 
-  def before_save(image)
+  def after_save(image)
     add_image_to_collection(image, attributes)
   end
 
@@ -47,10 +47,12 @@ class ImageFactory < ObjectFactory
   def add_image_to_collection(image, attrs)
     collection_attrs = attrs[:collection].merge(admin_policy_id: attributes[:admin_policy_id])
     CollectionFactory.new(collection_attrs).run do |coll|
-      image.collections << coll
+      coll.members << image
+      coll.save!
     end
-    # save the image to trigger a reindex with the collection association
-    image.save
+    # reload the collectoin association so we can reindex with the collection
+    image.association(:collections).reload
+    image.update_index
   end
 
 end

lib/importer/mods_importer.rb
@@ -21,8 +21,6 @@ module Importer
       puts "Importing: #{file}"
       parser = ModsParser.new(file)
       create_fedora_objects(parser.model, parser.attributes.merge(admin_policy_id: AdminPolicy::PUBLIC_POLICY_ID))
-    # rescue Oargun::RDF::Controlled::ControlledVocabularyError => e
-    #   puts "  Skipping, due to #{e.message}"
     end
 
     # Select a factory to create the objects in fedora.

lib/importer/mods_parser.rb
@@ -78,7 +78,7 @@ module Importer
         form_of_work: mods.genre.valueURI.map { |uri| RDF::URI.new(uri) },
         work_type: mods.typeOfResource.map(&:text),
         citation: citation,
-        note: note,
+        notes_attributes: notes,
         record_origin: record_origin,
         description_standard: mods.record_info.descriptionStandard.map(&:text)
       }
@@ -196,7 +196,7 @@ module Importer
       end
     end
 
-    def note
+    def notes
       preferred_citation = 'preferred citation'.freeze
       mods.note.each_with_object([]) do |node, list|
         next if node.attributes.has_key? preferred_citation

spec/importer/factories/image_factory_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 require 'importer'
 describe ImageFactory do
   let(:factory) { described_class.new(attributes) }
-  let(:collection_attrs) { { accession_number: ['SBHC Mss 36'] } }
-  let(:attributes) { { collection: collection_attrs, files: [], accession_number: ['123'] } }
+  let(:collection_attrs) { { accession_number: ["SBHC Mss 36"] } }
+
+  let(:attributes) do
+    {
+      collection: collection_attrs, files: [], accession_number: ['123'],
+      notes_attributes: [{:value=>"Title from item."}],
+      issued_attributes: [{ start: ["1925"], finish: [], label: [], start_qualifier: [], finish_qualifier: [] }]
+    }
+  end
+
+  # squelch output
+  before { allow($stdout).to receive(:puts) }
 
   context "when a collection already exists" do
-    let!(:coll) { Collection.create(collection_attrs.merge(id: 'fk44174k70')) }
+    let!(:coll) { Collection.create(collection_attrs) }
 
     it "should not create a new collection" do
       expect(coll.members.count).to eq 0

commit 44d5f0bd1f2387ff6fa69d6fcd01b892362a4cd5

Allow notes to be saved. Fixes #235 Bug was introduced in 9f4c897

lib/importer/factories/object_factory.rb
@@ -130,7 +130,6 @@ class ObjectFactory
     def transform_attributes
       contributors = find_or_create_contributors(klass.contributor_fields, attributes)
       rights_holders = find_or_create_rights_holders(attributes)
-      attributes.except(:note).merge(contributors).merge(rights_holders).
-        merge(notes_attributes: Array(attributes[:note]))
+      attributes.merge(contributors).merge(rights_holders)
     end
 end