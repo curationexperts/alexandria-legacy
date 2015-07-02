529f90c0047340a20cc09dee7bdb083a82bd3992
Justin Coyne <justin@curationexperts.com>
2015-07-02 15:12:02 -0500

    Show ETDs on the catalog page. Fixes #271

--- app/search_builders/search_builder.rb
+++ app/search_builders/search_builder.rb
@@ -3,6 +3,6 @@ class SearchBuilder < Hydra::SearchBuilder
 
   def only_visible_objects(solr_parameters)
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "{!terms f=has_model_ssim}#{Image.to_class_uri},Etd"
+    solr_parameters[:fq] << "{!terms f=has_model_ssim}#{Image.to_class_uri},ETD"
   end
 end
