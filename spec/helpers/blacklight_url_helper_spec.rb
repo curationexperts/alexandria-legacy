8a9f3c83ec71c8d7c94895f267c39e1c974840f1
Justin Coyne <justin@curationexperts.com>
2015-07-09 10:33:40 -0500

    ETDs should use the lib/ark: path for the show page

--- spec/helpers/blacklight_url_helper_spec.rb
+++ spec/helpers/blacklight_url_helper_spec.rb
@@ -4,19 +4,19 @@ describe BlacklightUrlHelper do
   describe "#url_for_document" do
     let(:document) { SolrDocument.new(has_model_ssim: [model], id: 'fk/4v/98/fk4v989d9j', identifier_ssm: ['ark:/99999/fk4v989d9j']) }
     context "with an image" do
-      let(:model) { 'Image' }
+      let(:model) { Image.to_class_uri }
       subject { helper.url_for_document(document) }
       it { is_expected.to eq '/lib/ark:/99999/fk4v989d9j' }
     end
 
     context "with an Etd" do
-      let(:model) { 'Etd' }
+      let(:model) { ETD.to_class_uri }
       subject { helper.url_for_document(document) }
       it { is_expected.to eq '/lib/ark:/99999/fk4v989d9j' }
     end
 
     context "with a collection" do
-      let(:model) { 'Collection' }
+      let(:model) { Collection.to_class_uri }
       subject { helper.url_for_document(document) }
       it { is_expected.to eq '/collections/fk4v989d9j' }
     end
