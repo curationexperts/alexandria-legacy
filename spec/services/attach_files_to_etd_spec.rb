d7e3cc9c271a1c6de1a701f1728a33e6dd1e0cdf
Justin Coyne <justin@curationexperts.com>
2015-07-08 21:02:11 -0500

    Zipfile service should return pdf, xml and supplemental files

--- spec/services/attach_files_to_etd_spec.rb
+++ spec/services/attach_files_to_etd_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 describe AttachFilesToETD do
   describe "run" do
-    let(:files) { ["Plunkett_ucsb_0035D_11862.pdf"] }
+    let(:file) { "Plunkett_ucsb_0035D_11862.pdf" }
     let(:zip_path) { 'fake_file.zip' }
     let(:etd) { ETD.create }
-    let(:file_hash) { { 'pdf' => "#{Rails.root}/spec/fixtures/pdf/sample.pdf",
-                        'xml' => "#{Rails.root}/spec/fixtures/proquest/Johnson_ucsb_0035N_12164_DATA.xml"
-                    } }
+    let(:file_struct) do
+      ZipfileService::ExtractedFiles.new(
+        "#{Rails.root}/spec/fixtures/pdf/sample.pdf",
+        "#{Rails.root}/spec/fixtures/proquest/Johnson_ucsb_0035N_12164_DATA.xml"
+      )
+    end
     before do
-      allow(ZipfileService).to receive(:find_file_containing).with(files.first).and_return(zip_path)
-      allow(ZipfileService).to receive(:extract_files).with(zip_path).and_return(file_hash)
-      described_class.run(etd, files)
+      allow_any_instance_of(ZipfileService).to receive(:extract_files).and_return(file_struct)
+      described_class.run(etd, file)
     end
 
     it "attaches files" do
--- spec/services/zipfile_service_spec.rb
+++ spec/services/zipfile_service_spec.rb
@@ -1,8 +1,34 @@
 require 'rails_helper'
 
 describe ZipfileService do
+  let(:service) { described_class.new('Murray_ucsb_0035D_12159.pdf') }
+
   describe "#wildcard_zip" do
-    subject { described_class.wildcard_zip }
+    subject { service.send(:wildcard_zip) }
     it { is_expected.to eq "#{Rails.root}/tmp/download_root/proquest/*.zip" }
+  end
+
+  describe "#extract_files" do
+    before do
+      allow(service).to receive(:find_zip_file).and_return('foo.zip')
+      allow(service).to receive(:run_unzip).and_return(
+        "Archive:  /opt/download_root/proquest/etdadmin_upload_292976.zip
+  inflating: /tmp/jcoyne/Murray_ucsb_0035D_12159.pdf
+  inflating: /tmp/jcoyne/Murray_ucsb_0035D_12159_DATA.xml
+  inflating: /tmp/jcoyne/SupplementalFile1.pdf
+  inflating: /tmp/jcoyne/SupplementalFile2.pdf
+  inflating: /tmp/jcoyne/SupplementalFile3.pdf
+")
+    end
+
+    subject { service.extract_files }
+    it { is_expected.to eq ZipfileService::ExtractedFiles.new(
+        '/tmp/jcoyne/Murray_ucsb_0035D_12159.pdf',
+        '/tmp/jcoyne/Murray_ucsb_0035D_12159_DATA.xml',
+        ['/tmp/jcoyne/SupplementalFile1.pdf',
+         '/tmp/jcoyne/SupplementalFile2.pdf',
+         '/tmp/jcoyne/SupplementalFile3.pdf']
+      ) }
+
   end
 end
