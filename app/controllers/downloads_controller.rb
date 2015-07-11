ffa87e2fdeae14606805385f0e1416e7ad95c90f
Justin Coyne <justin@curationexperts.com>
2015-07-10 21:43:41 -0500

    Add logging to downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,3 +1,12 @@
 class DownloadsController < ApplicationController
   include Hydra::Controller::DownloadBehavior
+
+  def asset
+    logger.info "Loading asset from id #{params[:id]}"
+    super
+  end
+
+  def load_file
+    asset.attached_files['original']
+  end
 end
