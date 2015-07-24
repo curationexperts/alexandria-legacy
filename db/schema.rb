0931ec7fb77d82474d52cbb3930b24a72c38f2d5
Justin Coyne <justin@curationexperts.com>
2015-07-24 12:41:10 -0500

    Fetch the groups from ldap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724131151) do
+ActiveRecord::Schema.define(version: 20150724165234) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -25,18 +25,6 @@ ActiveRecord::Schema.define(version: 20150724131151) do
 
   add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "roles_users", id: false, force: :cascade do |t|
-    t.integer "role_id"
-    t.integer "user_id"
-  end
-
-  add_index "roles_users", ["role_id", "user_id"], name: "index_roles_users_on_role_id_and_user_id"
-  add_index "roles_users", ["user_id", "role_id"], name: "index_roles_users_on_user_id_and_role_id"
-
   create_table "searches", force: :cascade do |t|
     t.text     "query_params"
     t.integer  "user_id"
@@ -48,17 +36,19 @@ ActiveRecord::Schema.define(version: 20150724131151) do
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",            default: "",    null: false
-    t.string   "encrypted_password",  default: "",    null: false
+    t.string   "username",               default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "guest",               default: false
+    t.boolean  "guest",                  default: false
+    t.text     "group_list"
+    t.datetime "groups_list_expires_at"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
