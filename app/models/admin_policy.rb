b525ae742de54b94298b3396f91df716f6ea8167
Justin Coyne <justin@curationexperts.com>
2015-07-24 16:02:23 -0500

    Use the metadata_admin group in the policies

--- app/models/admin_policy.rb
+++ app/models/admin_policy.rb
@@ -12,13 +12,13 @@ module AdminPolicy
     # TODO: Should ADMIN_USER_POLICY_ID be changed to something like RESTRICTED_POLICY_ID ?
     unless Hydra::AdminPolicy.exists?(ADMIN_USER_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: ADMIN_USER_POLICY_ID, title: ['Admin users only'])
-      policy.default_permissions.build(type: "group", name: "admin", access: "edit")
+      policy.default_permissions.build(type: "group", name: "metadata_admin", access: "edit")
       policy.save!
     end
 
     unless Hydra::AdminPolicy.exists?(DISCOVERY_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: DISCOVERY_POLICY_ID, title: ['Discovery access only'])
-      policy.default_permissions.build([ { type: "group", name: "admin", access: "edit" },
+      policy.default_permissions.build([ { type: "group", name: "metadata_admin", access: "edit" },
                                          { type: "group", name: "UCSB", access: "discover" },
                                          { type: "group", name: "public", access: "discover" } ])
       policy.save!
@@ -27,21 +27,21 @@ module AdminPolicy
     # TODO: Is this policy actually needed?
     unless Hydra::AdminPolicy.exists?(UCSB_CAMPUS_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: UCSB_CAMPUS_POLICY_ID, title: ['Campus use only, requires UCSB login'])
-      policy.default_permissions.build([ { type: "group", name: "admin", access: "edit" },
+      policy.default_permissions.build([ { type: "group", name: "metadata_admin", access: "edit" },
                                          { type: "group", name: "ucsb-on-campus", access: "read" } ])
       policy.save!
     end
 
     unless Hydra::AdminPolicy.exists?(UCSB_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: UCSB_POLICY_ID, title: ['UCSB users only'])
-      policy.default_permissions.build([ { type: "group", name: "admin", access: "edit" },
+      policy.default_permissions.build([ { type: "group", name: "metadata_admin", access: "edit" },
                                          { type: "group", name: "UCSB", access: "read" } ])
       policy.save!
     end
 
     unless Hydra::AdminPolicy.exists?(PUBLIC_CAMPUS_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: PUBLIC_CAMPUS_POLICY_ID, title: ['Campus use only'])
-      policy.default_permissions.build([ { type: "group", name: "admin", access: "edit" },
+      policy.default_permissions.build([ { type: "group", name: "metadata_admin", access: "edit" },
                                          { type: "group", name: "UCSB", access: "read" },
                                          { type: "group", name: "public-on-campus", access: "read" } ])
       policy.save!
@@ -49,7 +49,7 @@ module AdminPolicy
 
     unless Hydra::AdminPolicy.exists?(PUBLIC_POLICY_ID)
       policy = Hydra::AdminPolicy.create(id: PUBLIC_POLICY_ID, title: ['Public access'])
-      policy.default_permissions.build([ { type: "group", name: "admin", access: "edit" },
+      policy.default_permissions.build([ { type: "group", name: "metadata_admin", access: "edit" },
                                          { type: "group", name: "UCSB", access: "read" },
                                          { type: "group", name: "public", access: "read" } ])
       policy.save!
