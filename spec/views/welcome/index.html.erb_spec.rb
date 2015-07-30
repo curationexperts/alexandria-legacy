dc20dbf98a1a9a7e2a3d44fb923b5f7c20c12840
val99erie <val99erie@gmail.com>
2015-07-30 16:59:55 -0500

    Fixed a broken link

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "welcome/index.html.erb", :type => :view do
 
   it 'has a link to search for ETDs' do
     render
-    expect(rendered).to have_link 'electronic theses and dissertations', href: catalog_index_path('f[object_type_sim][]' => 'Thesis or dissertation')
+    expect(rendered).to have_link 'electronic theses and dissertations', href: catalog_index_path('f[active_fedora_model_ssi][]' => 'ETD')
   end
 
 end
