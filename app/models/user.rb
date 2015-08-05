beacb1403c200575d33ea0d98a6721255965c7a9
Justin Coyne <justin@curationexperts.com>
2015-08-05 11:35:48 -0500

    Remove user registration. Fixes #400

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :ldap_authenticatable, :registerable, :rememberable, :trackable
+  devise :ldap_authenticatable, :rememberable, :trackable
 
   serialize :group_list, Array
 
