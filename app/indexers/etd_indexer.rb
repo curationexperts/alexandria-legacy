077d4f50c57e85d79c0df781181660e49b26bc46
Justin Coyne <justin@curationexperts.com>
2015-08-05 15:55:57 -0500

    Import copyright. Fixes #349, Fixes #403

--- app/indexers/etd_indexer.rb
+++ app/indexers/etd_indexer.rb
@@ -2,6 +2,7 @@ class ETDIndexer < ObjectIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc[Solrizer.solr_name('generic_file_ids', :symbol)] = object.generic_file_ids
+      solr_doc[Solrizer.solr_name('copyright', :displayable)] = "#{object.rights_holder.first}, #{object.date_copyrighted.first}"
     end
   end
 
