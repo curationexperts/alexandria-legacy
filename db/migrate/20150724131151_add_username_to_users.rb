bd0d9ba68d9ee39e60dfdd9f04555074318bd130
val99erie <val99erie@gmail.com>
2015-08-14 12:28:06 -0500

    Fixed migration error. Story #470

--- db/migrate/20150724131151_add_username_to_users.rb
+++ db/migrate/20150724131151_add_username_to_users.rb
@@ -1,7 +1,7 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
-    rename_column :users, :email, :username
     rename_index :users, 'index_users_on_email', 'index_users_on_username'
+    rename_column :users, :email, :username
     remove_columns :users, :reset_password_token, :reset_password_sent_at
   end
 end
