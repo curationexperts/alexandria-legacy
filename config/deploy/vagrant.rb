fb90740f36cb750f0ff2acb8b0c354ff7ab72b18
Alex Dunn <dunn.alex@gmail.com>
2015-11-30 13:03:02 -0800

    try removing environment.rb

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -6,4 +6,6 @@ set :bundle_without, [:test]
 set :ssh_options, port: 2222, keys: ['~/.ssh/id_rsa']
 server ENV['SERVER'], user: 'deploy', roles: [:web, :app, :db]
 
-set :linked_files, %w(config/resque-pool.yml config/redis.yml config/blacklight.yml config/database.yml config/ezid.yml config/fedora.yml config/ldap.yml config/secrets.yml config/smtp.yml config/solr.yml config/environments/development.rb)
+# These files will be configured by the provisioning script in
+# PASSENGER_APP_ROOT/shared/
+set :linked_files, %w(config/resque-pool.yml config/redis.yml config/blacklight.yml config/database.yml config/ezid.yml config/fedora.yml config/ldap.yml config/secrets.yml config/smtp.yml config/solr.yml)
