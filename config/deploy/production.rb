bda454c01afd3d8a7ce32474582a8f6598c64b9e
Alex Dunn <dunn.alex@gmail.com>
2015-12-22 14:48:52 -0800

    Deploy from shared /adrlroot folder

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,19 @@ set :rails_env, 'production'
 server ENV['SERVER'], user: 'deploy', roles: [:web, :app, :db]
 set :ssh_options, port: 22, keys: ['~/.ssh/id_rsa']
 
-set :linked_files, %w(config/resque-pool.yml config/redis.yml config/blacklight.yml config/database.yml config/ezid.yml config/fedora.yml config/ldap.yml config/secrets.yml config/smtp.yml config/solr.yml config/environment.rb config/environments/production.rb)
+set :linked_files,
+    %w(
+      config/application.rb
+      config/blacklight.yml
+      config/database.yml
+      config/environment.rb
+      config/environments/production.rb
+      config/ezid.yml
+      config/fedora.yml
+      config/ldap.yml
+      config/redis.yml
+      config/resque-pool.yml
+      config/secrets.yml
+      config/smtp.yml
+      config/solr.yml
+    )
