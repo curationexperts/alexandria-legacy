771c80fda01fbdaebe042a499305bcf3bb33dd26
Justin Coyne <jcoyne@justincoyne.com>
2016-01-06 10:44:27 -0600

    Use HydraHead content negotiation. Fixes #283

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -5,19 +5,6 @@ describe SolrDocument do
     let(:image) { create(:image) }
     let(:document) { SolrDocument.new(id: image.id, identifier_ssm: ['ark:/99999/fk4v989d9j'], has_model_ssim: [Image.to_class_uri]) }
 
-    describe '#repository_model' do
-      subject { document.repository_model }
-      it 'converts to a model' do
-        expect(subject).to be_kind_of Image
-        expect(subject.resource).to be_kind_of ActiveTriples::Resource
-      end
-    end
-
-    describe '#export_as_ttl' do
-      subject { document.export_as_ttl }
-      it { is_expected.to match '<info:fedora/fedora-system:def/model#hasModel> "Image"' }
-    end
-
     describe '#ark' do
       subject { document.ark }
       it { is_expected.to eq 'ark:/99999/fk4v989d9j' }
