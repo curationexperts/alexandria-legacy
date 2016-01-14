5a04de483da7ec333a138e0dffe6b5079fbf8805
val99erie <val99erie@gmail.com>
2016-01-14 15:38:25 -0600

    CSV importer will create local authorities if they don't exist yet. Story DIGREPO-442

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveFedora::Base
   include LocalAuthorityBase
 
   property :label, predicate: ::RDF::SKOS.prefLabel do |index|
-    index.as :stored_searchable
+    index.as :stored_searchable, :symbol # Need :symbol for exact match for ObjectFactory find_or_create_* methods.
   end
 
   def to_param
