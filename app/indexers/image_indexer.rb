9d582591a1d4c571bbe632651a1b8877bf3e6b9b
Justin Coyne <jcoyne@justincoyne.com>
2016-02-09 11:31:01 -0600

    Freeze string literals to avoid additional instantiations

--- app/indexers/image_indexer.rb
+++ app/indexers/image_indexer.rb
@@ -6,8 +6,8 @@ class ImageIndexer < ObjectIndexer
       solr_doc['image_url_ssm'.freeze] = file_set_images
       solr_doc['large_image_url_ssm'.freeze] = file_set_large_images
       solr_doc[ISSUED] = issued
-      solr_doc[COPYRIGHTED] = display_date('date_copyrighted')
-      solr_doc['rights_holder_label_tesim'] = object['rights_holder'].flat_map(&:rdf_label)
+      solr_doc[COPYRIGHTED] = display_date('date_copyrighted'.freeze)
+      solr_doc['rights_holder_label_tesim'.freeze] = object['rights_holder'.freeze].flat_map(&:rdf_label)
     end
   end
 
@@ -21,7 +21,7 @@ class ImageIndexer < ObjectIndexer
       file_set_images('1000,'.freeze)
     end
 
-    def file_set_images(size = '600,')
+    def file_set_images(size = '600,'.freeze)
       object.file_sets.map do |file_set|
         file = file_set.files.first
         next unless file
@@ -35,7 +35,7 @@ class ImageIndexer < ObjectIndexer
 
     def host
       hostname = Rails.application.config.host_name
-      if hostname == 'localhost' || hostname == '127.0.0.1'
+      if hostname == 'localhost'.freeze || hostname == '127.0.0.1'.freeze
         "#{hostname}:#{Rails.application.secrets.localhost_port}"
       else
         hostname
