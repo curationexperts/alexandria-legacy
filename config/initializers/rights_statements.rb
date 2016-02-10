f37a96098f75534073f0e92948e0408605347f84
Justin Coyne <jcoyne@justincoyne.com>
2016-02-10 09:55:48 -0600

    Store a label for the unknown rights statement. Fixes #521

---
+++ config/initializers/rights_statements.rb
@@ -0,0 +1,6 @@
+# Europeana rights statements are not returning labels,
+# this puts a valid label into Marmotta so we don't hit
+# Europeana multiple times without effect.
+stmt = Oargun::ControlledVocabularies::RightsStatement.new('http://www.europeana.eu/rights/unknown/')
+stmt << RDF::Statement.new(stmt.rdf_subject, RDF::Vocab::SKOS.prefLabel, 'Unknown copyright status')
+stmt.persist!
