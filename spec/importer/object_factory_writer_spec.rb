65140cddf79636da2938de32ee6869b55f72fef5
Justin Coyne <jcoyne@justincoyne.com>
2016-02-11 13:22:01 -0600

    Remove duplicate key

--- spec/importer/object_factory_writer_spec.rb
+++ spec/importer/object_factory_writer_spec.rb
@@ -28,7 +28,6 @@ describe ObjectFactoryWriter do
         'dissertation_institution' => [nil],
         'dissertation_year' => [nil],
         'fulltext_link' => [nil],
-        'fulltext_link' => [nil],
         'work_type' => [RDF::URI('http://id.loc.gov/vocabulary/resourceTypes/txt')]
       }
     end
