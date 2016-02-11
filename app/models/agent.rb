419c0d6b09d2886e0a609d751d113d7215289c20
val99erie <val99erie@gmail.com>
2016-02-11 11:42:59 -0600

    Importers should use public URI instead of fedora URI. Story #545

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -5,7 +5,16 @@ class Agent < ActiveFedora::Base
   include LocalAuthorityBase
 
   rdf_label ::RDF::Vocab::FOAF.name
+
   property :foaf_name, predicate: ::RDF::Vocab::FOAF.name, multiple: false do |index|
     index.as :stored_searchable, :symbol # Need :symbol for exact match for ObjectFactory find_or_create_* methods.
   end
+
+  property :label, predicate: ::RDF::RDFS.label
+  before_save :sync_label
+
+  def sync_label
+    self.label = Array(foaf_name)
+  end
+
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 describe Person do
+  let(:name) { 'Justin' }
+  let(:person) { described_class.new(foaf_name: name) }
+
+  describe '#label' do
+    before { AdminPolicy.ensure_admin_policy_exists }
+
+    it 'saves a label that is the same as the foaf_name' do
+      expect(person.label).to eq []
+      person.save!
+      expect(person.label).to eq [name]
+    end
+  end
+
   describe '#rdf_label' do
-    let(:person) { described_class.new(foaf_name: 'Justin') }
     subject { person.rdf_label }
     it { is_expected.to eq ['Justin'] }
   end
@@ -25,8 +37,6 @@ describe Person do
   end
 
   describe '#public_uri' do
-    let(:person) { described_class.new(foaf_name: 'Justin') }
-
     context "when the Person hasn't been saved yet" do
       it 'returns nil' do
         expect(person.public_uri).to be_nil
