6736823e52b431d30af3d964944ffb77f697b5e1
Justin Coyne <jcoyne@justincoyne.com>
2016-02-12 09:15:37 -0600

    Remove dead code

--- app/utils/identifier.rb
+++ app/utils/identifier.rb
@@ -1,16 +1,7 @@
+# Utilities for tranforming noids to ARKs and Fedora treeified paths
 module Identifier
-  def self.treeify(identifier)
-    raise "Treeify?"
-    (identifier.scan(/..?/).first(4) + [identifier]).join('/')
-  end
-
-  def self.noidify(id)
-    raise "Noidify?"
-    id.to_s.split('/')[-1]
-  end
-
   def self.ark_to_noid(ark)
-    if matches = /^ark:\/\d{5}\/(f\w{7,9})$/.match(ark)
+    if matches = %r{^ark:/\d{5}/(f\w{7,9})$}.match(ark)
       matches[1]
     end
   end
