149842ee00f8c57a8846823fc78575cea285b0f0
Justin Coyne <jcoyne@justincoyne.com>
2016-02-19 15:05:58 -0600

    Create an audio with a license (instead of rights)

--- app/forms/curation_concerns/audio_recording_form.rb
+++ app/forms/curation_concerns/audio_recording_form.rb
@@ -1,8 +1,16 @@
-# Generated via
-#  `rails generate curation_concerns:work AudioRecording`
 module CurationConcerns
   class AudioRecordingForm < CurationConcerns::Forms::WorkForm
     self.model_class = ::AudioRecording
+    delegate :license, to: :model
+
+    self.terms -= [:rights]
+    self.terms += [:license]
+
+    # Overriden to cast 'license' to an array
+    def self.sanitize_params(form_params)
+      form_params['license'] = Array(form_params['license']) if form_params.key?('license')
+      super(form_params)
+    end
   end
 end
 
