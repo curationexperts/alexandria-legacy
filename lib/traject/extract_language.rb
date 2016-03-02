1eac38fc0476cd65c2057ebaed380beefd8887de
Justin Coyne <jcoyne@justincoyne.com>
2016-03-01 21:34:17 -0600

    Handle cylinders without language. Fixes #630

--- lib/traject/extract_language.rb
+++ lib/traject/extract_language.rb
@@ -4,6 +4,8 @@ module ExtractLanguage
     lang_extractor = Traject::MarcExtractor.new('008[35-37]')
     lambda do |record, accumulator|
       abbrev = lang_extractor.extract(record).compact.first
+      # Check for empty strings
+      return if /\s/.match abbrev
       accumulator << RDF::URI("http://id.loc.gov/vocabulary/iso639-2/#{abbrev}")
     end
   end
--- spec/importer/traject_spec.rb
+++ spec/importer/traject_spec.rb
@@ -3,14 +3,13 @@ require 'traject/command_line'
 
 describe 'Traject importer' do
   describe 'import a cylinder record' do
-    let(:marc_file) { File.join(fixture_path, 'marcxml', 'cylinder_sample_marc.xml') }
     let(:traject_config) { File.join(Rails.root, 'lib', 'traject', 'audio_config.rb') }
     let(:command) { Traject::CommandLine.new(['-c', traject_config, marc_file]) }
 
+    let!(:collection) { Collection.create!(title: ['Wax Cylinders'], accession_number: ['Cylinders']) }
+
     before do
-      # IDs from the ezid VCR cassette and MARC file
-      ids = %w(f3999999 fk4c252k0f)
-      ids.each { |id| ActiveFedora::Base.find(id).destroy(eradicate: true) if ActiveFedora::Base.exists? id }
+      ActiveFedora::Base.find(id).destroy(eradicate: true) if ActiveFedora::Base.exists? id
 
       AudioRecording.destroy_all
       Organization.destroy_all
@@ -25,51 +24,73 @@ describe 'Traject importer' do
       allow_any_instance_of(RDF::DeepIndexingService).to receive(:fetch_external)
     end
 
-    it 'creates the audio record' do
-      VCR.use_cassette('ezid') do
-        result = command.execute
+    context "marcfile with language" do
+      let(:marc_file) { File.join(fixture_path, 'marcxml', 'cylinder_sample_marc.xml') }
+      # IDs from the MARC file
+      let(:id) { 'f3999999' }
+
+      it 'creates the audio record' do
+        expect(command.execute).to be true
+
+        # These new records should have been created
+        expect(AudioRecording.count).to eq 1
+        expect(Person.count).to eq 2
+        expect(Group.count).to eq 1
+        expect(Organization.count).to eq 2
+
+        audio = AudioRecording.first
+
+        expect(audio.language.first.rdf_subject).to eq RDF::URI('http://id.loc.gov/vocabulary/iso639-2/eng')
+        expect(audio.in_collections).to eq [collection]
+
+        # Check all the contributors are correct
+        [:performer, :instrumentalist, :lyricist, :arranger, :singer].each do |property_name|
+          contributor = audio.send(property_name)
+          expect(contributor.map(&:class).uniq).to eq [Oargun::ControlledVocabularies::Creator]
+        end
+
+        # Check local authorities were created for performers
+        ids = audio.performer.map { |s| s.rdf_label.first.gsub(Regexp.new('^.*\/'), '') }
+        perfs = ids.map { |id| ActiveFedora::Base.find(id) }
+        org, group = perfs.partition { |obj| obj.is_a?(Organization) }.map(&:first)
+        expect(org.foaf_name).to eq 'United States. National Guard Bureau. Fife and Drum Corps.'
+        expect(group.foaf_name).to eq 'Allen field c text 1876 field q text'
+        expect(group.class).to eq Group
+
+        # Check local authorities were created for singers
+        ids = audio.singer.map { |s| s.rdf_label.first.gsub(Regexp.new('^.*\/'), '') }
+        singers = ids.map { |id| ActiveFedora::Base.find(id) }
+        org, person = singers.partition { |obj| obj.is_a?(Organization) }.map(&:first)
+        expect(org.foaf_name).to eq 'Louisiana Five. text from b.'
+        expect(person.foaf_name).to eq 'Collins, Arthur.'
+        expect(person.class).to eq Person
+
+        # This is the same person who is listed as 3 different
+        # types of contributor.
+        person_id = audio.instrumentalist.first.rdf_label.first.gsub(Regexp.new('^.*\/'), '')
+        person = Person.find(person_id)
+        expect(person.foaf_name).to eq 'Allen, Thos. S., 1876-1919.'
+
+        [:instrumentalist, :lyricist, :arranger].each do |property_name|
+          contributor = audio.send(property_name)
+          contributor_id = contributor.first.rdf_label.first.gsub(Regexp.new('^.*\/'), '')
+          expect(contributor_id).to eq person.id
+        end
       end
+    end
 
-      # These new records should have been created
-      expect(AudioRecording.count).to eq 1
-      expect(Person.count).to eq 2
-      expect(Group.count).to eq 1
-      expect(Organization.count).to eq 2
+    context "marc file without language" do
+      let(:marc_file) { File.join(fixture_path, 'marcxml', 'cylinder_sample_without_language_marc.xml') }
+      # IDs from the MARC file
+      let(:id) { 'f3888888' }
 
-      audio = AudioRecording.first
+      it 'creates the audio record' do
+        expect(command.execute).to be true
 
-      # Check all the contributors are correct
-      [:performer, :instrumentalist, :lyricist, :arranger, :singer].each do |property_name|
-        contributor = audio.send(property_name)
-        expect(contributor.map(&:class).uniq).to eq [Oargun::ControlledVocabularies::Creator]
-      end
-
-      # Check local authorities were created for performers
-      ids = audio.performer.map { |s| s.rdf_label.first.gsub(Regexp.new('^.*\/'), '') }
-      perfs = ids.map { |id| ActiveFedora::Base.find(id) }
-      org, group = perfs.partition { |obj| obj.is_a?(Organization) }.map(&:first)
-      expect(org.foaf_name).to eq 'United States. National Guard Bureau. Fife and Drum Corps.'
-      expect(group.foaf_name).to eq 'Allen field c text 1876 field q text'
-      expect(group.class).to eq Group
-
-      # Check local authorities were created for singers
-      ids = audio.singer.map { |s| s.rdf_label.first.gsub(Regexp.new('^.*\/'), '') }
-      singers = ids.map { |id| ActiveFedora::Base.find(id) }
-      org, person = singers.partition { |obj| obj.is_a?(Organization) }.map(&:first)
-      expect(org.foaf_name).to eq 'Louisiana Five. text from b.'
-      expect(person.foaf_name).to eq 'Collins, Arthur.'
-      expect(person.class).to eq Person
-
-      # This is the same person who is listed as 3 different
-      # types of contributor.
-      person_id = audio.instrumentalist.first.rdf_label.first.gsub(Regexp.new('^.*\/'), '')
-      person = Person.find(person_id)
-      expect(person.foaf_name).to eq 'Allen, Thos. S., 1876-1919.'
-
-      [:instrumentalist, :lyricist, :arranger].each do |property_name|
-        contributor = audio.send(property_name)
-        contributor_id = contributor.first.rdf_label.first.gsub(Regexp.new('^.*\/'), '')
-        expect(contributor_id).to eq person.id
+        # These new records should have been created
+        expect(AudioRecording.count).to eq 1
+        audio = AudioRecording.first
+        expect(audio.language).to eq []
       end
     end
   end
