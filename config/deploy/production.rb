368b75ef90b9f56e8c3c65c611ef455680188cfb
Alex Dunn <adunn@ucsb.edu>
2016-03-14 08:38:45 -0700

    [deploy] make more vars configurable

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 set :rails_env, 'production'
 
-server ENV['SERVER'], user: 'adrl', roles: [:web, :app, :db, :resque_pool]
-set :ssh_options, port: 22, keys: ['~/.ssh/id_rsa']
+server ENV['SERVER'], user: ENV.fetch('USER', 'adrl'), roles: [:web, :app, :db, :resque_pool]
+set :ssh_options, port: 22, keys: [ENV.fetch('KEYFILE', '~/.ssh/id_rsa')]
